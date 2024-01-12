21fd1584ce94c7fcd07fb8264fe3b860b9bd1059
Devang kushwah <deekayedm@gmail.com>
2024-01-12 15:01:33 +0530

    register page done

--- Frontend/src/Signup.jsx
+++ Frontend/src/Signup.jsx
@@ -15,7 +15,7 @@ function Signup(){
                         autoComplete="off"
                         placeholder="Enter Name"
                         name="name"
-                        className="form-control rounded-2"
+                        className="form-control rounded-0"
                         >
                         </input>
                     </div>
@@ -28,7 +28,7 @@ function Signup(){
                         placeholder="Enter Email"
                         autoComplete="off"
                         name="email"
-                        className="form-control rounded-2">
+                        className="form-control rounded-0">
                         </input>
                     </div>
                     <div className="m-3">
@@ -38,9 +38,14 @@ function Signup(){
                         <input
                         type="password"
                         placeholder="Enter Password"
-                        
-                        className="form-control"></input>
+                        autoComplete="off"
+                        name="password"
+                        className="form-control rounded-0"></input>
                     </div>
+                    <button tyoe="sumbit" className="w-100 btn btn-success rounded-2">Register
+                    </button>
+                    <p>Already Have an Account</p>
+                    <button className="btn btn-default border w-100 bg-light rounded-2 text-decoration-none">Login</button>
                 </form>
 
             </div>
